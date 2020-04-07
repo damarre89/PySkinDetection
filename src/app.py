4cdd3571ed8016e297404ba15b77a60844e687f0
damarre89 <45032526+damarre89@users.noreply.github.com>
2020-04-07 10:19:14 +0200

    Update app.py
    modifications to get webcam as input

--- src/app.py
+++ src/app.py
@@ -1,8 +1,20 @@
-import sys # for Command Line Arguments
+import cv2 # for webcam access
 from jeanCV import skinDetector
 
 
-imageName = sys.argv[1]
+def show_webcam():
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret, img = cam.read()
+        cv2.imshow('input', img)
+        detector = skinDetector(img)
+        detector.find_skin()
+        if cv2.waitKey(250) == 27:
+            break # esc to exit
+    cv2.destroyAllWindows()
 
-detector = skinDetector(imageName)
-detector.find_skin()
+def main():
+    show_webcam()
+
+if __name__ == '__main__':
+    main()
